e4544e136a2f76d7c1434229454650694d0ca11b
Padraig Gleeson <p.gleeson@gmail.com>
2018-06-01 15:36:18 +0100

    Retested & generated

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 set -e
 
+##   Install
+
+sudo python setup.py install
+
 ##   (Re)generate NeuroML 2 & LEMS files from the python scripts
 
 python c302/c302_IClamp.py A
@@ -44,13 +48,13 @@ python c302/c302_Full.py C1
 python c302/c302_Muscles.py C1
 python c302/c302_Oscillator.py C1
 
-#python c302/c302_IClamp.py C2
-#python c302/c302_Syns.py C2
-#python c302/c302_Social.py C2
-#python c302/c302_Pharyngeal.py C2
-#python c302/c302_Full.py C2
-#python c302/c302_Muscles.py C2
-#python c302/c302_Oscillator.py C2
+python c302/c302_IClamp.py C2
+python c302/c302_Syns.py C2
+python c302/c302_Social.py C2
+python c302/c302_Pharyngeal.py C2
+python c302/c302_Full.py C2
+python c302/c302_Muscles.py C2
+python c302/c302_Oscillator.py C2
 
 python c302/c302_IClamp.py D
 python c302/c302_Syns.py D
