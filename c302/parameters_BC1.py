9aff9747effdb33e5a2c246343d6a78b92bebe86
Padraig Gleeson <p.gleeson@gmail.com>
2020-12-15 16:07:16 +0000

    Regenerated and tested

--- c302/parameters_BC1.py
+++ c302/parameters_BC1.py
@@ -4,25 +4,11 @@
         Cells:           Simple integrate and fire cells, custom component type, with an "activity" variable (same as B)
         Chem Synapses:   Analogue/graded synapses; continuous transmission (voltage dependent) (same as C1)
         Gap junctions:   Electrical connection; current linerly depends on difference in voltages 
-        
+
     ASSESSMENT:
         Probably not very useful in longer term; same criticisms as parameters A; see C0
 
 '''
-
-from neuroml import Cell
-from neuroml import Morphology
-from neuroml import Point3DWithDiam
-from neuroml import Segment
-from neuroml import BiophysicalProperties
-from neuroml import IntracellularProperties
-from neuroml import Resistivity
-from neuroml import Species
-from neuroml import MembraneProperties
-from neuroml import InitMembPotential
-from neuroml import SpecificCapacitance
-from neuroml import ChannelDensity
-from neuroml import SpikeThresh
 
 from neuroml import GradedSynapse
 from neuroml import GapJunction
@@ -39,7 +25,7 @@ class ParameterisedModel(c302ModelPrototype):
         super(ParameterisedModel, self).__init__()
         self.level = "BC1"
         self.custom_component_types_definitions = 'cell_B.xml'
-        
+
         self.set_default_bioparameters()
 
     def set_default_bioparameters(self):
@@ -74,7 +60,7 @@ class ParameterisedModel(c302ModelPrototype):
         self.add_bioparameter("inh_syn_vth", "0 mV", "BlindGuess", "0.1")
         self.add_bioparameter("inh_syn_erev", "-70 mV", "BlindGuess", "0.1")
         self.add_bioparameter("inh_syn_k", "0.025per_ms", "BlindGuess", "0.1")
-        
+
 
         self.add_bioparameter("neuron_to_neuron_elec_syn_gbase", "0.3 nS", "BlindGuess", "0.1")
         self.add_bioparameter("neuron_to_muscle_elec_syn_gbase", "0.3 nS", "BlindGuess", "0.1")
@@ -89,15 +75,17 @@ class ParameterisedModel(c302ModelPrototype):
                                 thresh =            self.get_bioparameter("muscle_iaf_thresh").value,
                                 reset =             self.get_bioparameter("muscle_iaf_reset").value,
                                 leak_conductance =  self.get_bioparameter("muscle_iaf_conductance").value,
-                                leak_reversal =     self.get_bioparameter("muscle_iaf_leak_reversal").value)   
-   
+                                leak_reversal =     self.get_bioparameter("muscle_iaf_leak_reversal").value,
+                                tau1 =              self.get_bioparameter("muscle_iaf_tau1").value)  
+
     def create_generic_neuron_cell(self):
         self.generic_neuron_cell = IafActivityCell(id="generic_neuron_iaf_cell", 
                                 C =                 self.get_bioparameter("neuron_iaf_C").value,
                                 thresh =            self.get_bioparameter("neuron_iaf_thresh").value,
                                 reset =             self.get_bioparameter("neuron_iaf_reset").value,
                                 leak_conductance =  self.get_bioparameter("neuron_iaf_conductance").value,
-                                leak_reversal =     self.get_bioparameter("neuron_iaf_leak_reversal").value)  
+                                leak_reversal =     self.get_bioparameter("neuron_iaf_leak_reversal").value,
+                                tau1 =              self.get_bioparameter("neuron_iaf_tau1").value)   
 
     def create_offset(self):
 
@@ -125,7 +113,7 @@ class ParameterisedModel(c302ModelPrototype):
         self.neuron_to_neuron_elec_syn = GapJunction(id="neuron_to_neuron_elec_syn",
                                conductance =    self.get_bioparameter("neuron_to_neuron_elec_syn_gbase").value)
 
-   def create_neuron_to_muscle_syn(self):
+    def create_neuron_to_muscle_syn(self):
         self.neuron_to_muscle_exc_syn = GradedSynapse(id="neuron_to_muscle_exc_syn",
                                 conductance =        self.get_bioparameter("neuron_to_muscle_exc_syn_conductance").value,
                                 delta =              self.get_bioparameter("exc_syn_delta").value,
