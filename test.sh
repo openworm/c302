501fc6205827234b36be8f8320164f48224db767
yasinthanvickneswaran <147663006+yasinthanvickneswaran@users.noreply.github.com>
2023-12-19 02:52:07 +0000

    Update test.sh to include WormNeuroAtlasReader

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ python -m c302.SpreadsheetDataReader
 python -m c302.UpdatedSpreadsheetDataReader                                                                                                                                                                      
 python -m c302.UpdatedSpreadsheetDataReader2                                                                                                                                                                  
 python -m c302.OpenWormReader 
+python -m c302.WormNeuroAtlasReader
 
 
 ##   (Re)generate NeuroML 2 & LEMS files from the python scripts
