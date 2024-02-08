0eff090d30a466d61554c206e105543b423e51c6
yasinthanvickneswaran <v.yasinthan@outlook.com>
2024-02-08 13:33:48 +0000

    VarshneyDataReader

--- c302/VarshneyDataReader.py
+++ c302/VarshneyDataReader.py
@@ -8,7 +8,7 @@ spreadsheet_location = os.path.dirname(os.path.abspath(__file__))+"/data/"
 
 from c302 import print_
 
-READER_DESCRIPTION = """Data extracted from CElegansNeuronTables.xls for neuronal connectivity"""
+READER_DESCRIPTION = """Data extracted from NeuronConnectFormatted.xls for neuronal connectivity"""
 
 def read_data(include_nonconnected_cells=False, neuron_connect=True):
 
