44feb88ec49e665fe27a5e599bca35c9a56621cf
pgleeson <p.gleeson@gmail.com>
2024-08-29 15:28:04 +0100

    To v0.8.1; apply black formatting to python

--- c302/Cook2019DataReader.py
+++ c302/Cook2019DataReader.py
@@ -4,7 +4,7 @@
 
 #    A simple script to read the values in herm_full_edgelist.csv.
 
-#    This is on of a number of interchangeable "Readers" which can 
+#    This is on of a number of interchangeable "Readers" which can
 #    be used to get connection data for c302
 
 ############################################################
@@ -25,21 +25,19 @@ import numpy as np
 
 from c302 import print_
 
-spreadsheet_location = os.path.dirname(os.path.abspath(__file__))+"/data/"
+spreadsheet_location = os.path.dirname(os.path.abspath(__file__)) + "/data/"
 filename = "%sSI 5 Connectome adjacency matrices.xlsx" % spreadsheet_location
 
-HERM_CHEM = 'hermaphrodite chemical'
-HERM_GAP_SYMM = 'herm gap jn symmetric'
+HERM_CHEM = "hermaphrodite chemical"
+HERM_GAP_SYMM = "herm gap jn symmetric"
 
-pre_range = {HERM_CHEM:range(4,304),
-             HERM_GAP_SYMM:range(4,472)}
-             
-post_range = {HERM_CHEM:range(4,457),
-             HERM_GAP_SYMM:range(4,472)}
+pre_range = {HERM_CHEM: range(4, 304), HERM_GAP_SYMM: range(4, 472)}
+
+post_range = {HERM_CHEM: range(4, 457), HERM_GAP_SYMM: range(4, 472)}
 
 
 def get_synclass(cell, syntype):
-    #TODO: fix this dirty hack
+    # TODO: fix this dirty hack
     if syntype == "GapJunction":
         return "Generic_GJ"
     else:
@@ -47,51 +45,74 @@ def get_synclass(cell, syntype):
             return "GABA"
         return "Acetylcholine"
 
-class Cook2019DataReader():
 
+class Cook2019DataReader:
     verbose = False
 
     def __init__(self):
-
         wb = load_workbook(filename)
-        print_("Opened the Excel file: " + filename)   
+        print_("Opened the Excel file: " + filename)
 
         self.pre_cells = {}
         self.post_cells = {}
         self.conn_nums = {}
 
         for conn_type in [HERM_CHEM, HERM_GAP_SYMM]:
-
             sheet = wb.get_sheet_by_name(conn_type)
-            print('Looking at sheet: %s'%conn_type)
+            print("Looking at sheet: %s" % conn_type)
 
             self.pre_cells[conn_type] = []
             self.post_cells[conn_type] = []
 
             for i in pre_range[conn_type]:
-                self.pre_cells[conn_type].append(sheet['C%i'%i].value)
+                self.pre_cells[conn_type].append(sheet["C%i" % i].value)
 
-            if self.verbose: print(' - Pre cells for %s (%i):\n%s'%(conn_type, len(self.pre_cells[conn_type]), self.pre_cells[conn_type]))
+            if self.verbose:
+                print(
+                    " - Pre cells for %s (%i):\n%s"
+                    % (
+                        conn_type,
+                        len(self.pre_cells[conn_type]),
+                        self.pre_cells[conn_type],
+                    )
+                )
 
             for i in post_range[conn_type]:
                 self.post_cells[conn_type].append(sheet.cell(row=3, column=i).value)
 
-            if self.verbose: print(' - Post cells for %s (%i):\n%s'%(conn_type, len(self.post_cells[conn_type]), self.post_cells[conn_type]))
+            if self.verbose:
+                print(
+                    " - Post cells for %s (%i):\n%s"
+                    % (
+                        conn_type,
+                        len(self.post_cells[conn_type]),
+                        self.post_cells[conn_type],
+                    )
+                )
 
-            self.conn_nums[conn_type] = np.zeros([len(self.pre_cells[conn_type]),len(self.post_cells[conn_type])], dtype=int)
+            self.conn_nums[conn_type] = np.zeros(
+                [len(self.pre_cells[conn_type]), len(self.post_cells[conn_type])],
+                dtype=int,
+            )
 
             for i in range(len(self.pre_cells[conn_type])):
                 for j in range(len(self.post_cells[conn_type])):
-                    row =4+i
-                    col = 4+j
+                    row = 4 + i
+                    col = 4 + j
                     val = sheet.cell(row=row, column=col).value
-                    #print('Cell (%i,%i) [row %i, col %i] = %s'%(i,j,row, col, val))
+                    # print('Cell (%i,%i) [row %i, col %i] = %s'%(i,j,row, col, val))
                     if val is not None:
-                        self.conn_nums[conn_type][i,j] = int(val)
+                        self.conn_nums[conn_type][i, j] = int(val)
 
-            if self.verbose: print(' - Conns for %s (%s):\n%s'%(conn_type, self.conn_nums[conn_type].shape, self.conn_nums[conn_type]))
-
-
+            if self.verbose:
+                print(
+                    " - Conns for %s (%s):\n%s"
+                    % (
+                        conn_type,
+                        self.conn_nums[conn_type].shape,
+                        self.conn_nums[conn_type],
+                    )
+                )
 
     def read_data(self, include_nonconnected_cells=False):
         """
@@ -103,35 +124,36 @@ class Cook2019DataReader():
         """
 
         if not include_nonconnected_cells:
-            raise Exception('Option include_nonconnected_cells=False not supported')
+            raise Exception("Option include_nonconnected_cells=False not supported")
 
         conns = []
         cells = []
         for conn_type in [HERM_CHEM, HERM_GAP_SYMM]:
-
             for pre_index in range(len(self.pre_cells[conn_type])):
                 for post_index in range(len(self.post_cells[conn_type])):
-
-                    pre = remove_leading_index_zero(self.pre_cells[conn_type][pre_index])
-                    post = remove_leading_index_zero(self.post_cells[conn_type][post_index])
+                    pre = remove_leading_index_zero(
+                        self.pre_cells[conn_type][pre_index]
+                    )
+                    post = remove_leading_index_zero(
+                        self.post_cells[conn_type][post_index]
+                    )
 
                     if is_body_wall_muscle(post):
                         continue  # post is a BWM so ignore
 
-                    num = self.conn_nums[conn_type][pre_index,post_index]
-                    if num>0:
-                        syntype = 'Send' if conn_type == HERM_CHEM else "GapJunction"
+                    num = self.conn_nums[conn_type][pre_index, post_index]
+                    if num > 0:
+                        syntype = "Send" if conn_type == HERM_CHEM else "GapJunction"
                         synclass = get_synclass(pre, syntype)
 
                         conns.append(ConnectionInfo(pre, post, num, syntype, synclass))
-                        #print ConnectionInfo(pre, post, num, syntype, synclass)
+                        # print ConnectionInfo(pre, post, num, syntype, synclass)
                         if pre not in cells:
                             cells.append(pre)
                         if post not in cells:
                             cells.append(post)
 
         return cells, conns
-
 
     def read_muscle_data(self):
         """
@@ -145,7 +167,7 @@ class Cook2019DataReader():
         muscles = []
         conns = []
 
-        '''
+        """
         with open(filename, 'r') as f:
             reader = csv.DictReader(f)
             print_("Opened file: " + filename)
@@ -168,21 +190,21 @@ class Cook2019DataReader():
                 elif is_body_wall_muscle(pre) and pre not in muscles:
                     muscles.append(pre)
                 if post not in muscles:
-                    muscles.append(post)''' 
+                    muscles.append(post)"""
 
         return neurons, muscles, conns
 
 
 def main():
-
     cdr = Cook2019DataReader()
     read_data = cdr.read_data
     read_muscle_data = cdr.read_muscle_data
-    
+
     cells, neuron_conns = read_data(include_nonconnected_cells=True)
     neurons2muscles, muscles, muscle_conns = read_muscle_data()
 
     analyse_connections(cells, neuron_conns, neurons2muscles, muscles, muscle_conns)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- c302/c302_Pharyngeal.py
+++ c302/c302_Pharyngeal.py
@@ -1,46 +1,88 @@
 import c302
 import sys
 
-def setup(parameter_set,
-          generate=False,
-          duration=500,
-          dt=0.01,
-          target_directory='examples',
-          data_reader="SpreadsheetDataReader",
-          param_overrides={},
-          config_param_overrides={},
-          verbose=True):
 
-    exec('from c302.parameters_%s import ParameterisedModel'%parameter_set, globals())
+def setup(
+    parameter_set,
+    generate=False,
+    duration=500,
+    dt=0.01,
+    target_directory="examples",
+    data_reader="SpreadsheetDataReader",
+    param_overrides={},
+    config_param_overrides={},
+    verbose=True,
+):
+    exec("from c302.parameters_%s import ParameterisedModel" % parameter_set, globals())
     params = ParameterisedModel()
 
-    params.set_bioparameter("unphysiological_offset_current", "2.2pA", "Testing Pharyngeal", "0")
-    params.set_bioparameter("unphysiological_offset_current_del", "50ms", "Testing Pharyngeal", "0")
-    params.set_bioparameter("unphysiological_offset_current_dur", "200ms", "Testing Pharyngeal", "0")
+    params.set_bioparameter(
+        "unphysiological_offset_current", "2.2pA", "Testing Pharyngeal", "0"
+    )
+    params.set_bioparameter(
+        "unphysiological_offset_current_del", "50ms", "Testing Pharyngeal", "0"
+    )
+    params.set_bioparameter(
+        "unphysiological_offset_current_dur", "200ms", "Testing Pharyngeal", "0"
+    )
 
-    cells = ["M1","M2L","M2R","M3L","M3R","M4","M5","I1L","I1R","I2L","I2R","I3","I4","I5","I6","MI","NSML","NSMR","MCL","MCR"]
-    cells_to_stimulate = ["M1","M3R","M4","M5","I1L","I4","I5","I6","MCL","MCR"]
+    cells = [
+        "M1",
+        "M2L",
+        "M2R",
+        "M3L",
+        "M3R",
+        "M4",
+        "M5",
+        "I1L",
+        "I1R",
+        "I2L",
+        "I2R",
+        "I3",
+        "I4",
+        "I5",
+        "I6",
+        "MI",
+        "NSML",
+        "NSMR",
+        "MCL",
+        "MCR",
+    ]
+    cells_to_stimulate = [
+        "M1",
+        "M3R",
+        "M4",
+        "M5",
+        "I1L",
+        "I4",
+        "I5",
+        "I6",
+        "MCL",
+        "MCR",
+    ]
 
-    reference = "c302_%s_Pharyngeal"%parameter_set
+    reference = "c302_%s_Pharyngeal" % parameter_set
 
     nml_doc = None
 
     if generate:
-        nml_doc = c302.generate(reference,
-                      params,
-                      cells=cells,
-                      cells_to_stimulate=cells_to_stimulate,
-                      duration=duration,
-                      dt=dt,
-                      target_directory=target_directory,
-                      param_overrides=param_overrides,
-                      verbose=verbose,
-                      data_reader=data_reader)
+        nml_doc = c302.generate(
+            reference,
+            params,
+            cells=cells,
+            cells_to_stimulate=cells_to_stimulate,
+            duration=duration,
+            dt=dt,
+            target_directory=target_directory,
+            param_overrides=param_overrides,
+            verbose=verbose,
+            data_reader=data_reader,
+        )
 
     return cells, cells_to_stimulate, params, [], nml_doc
 
-if __name__ == '__main__':
 
-    parameter_set = sys.argv[1] if len(sys.argv)==2 else 'A'
+if __name__ == "__main__":
+    parameter_set = sys.argv[1] if len(sys.argv) == 2 else "A"
 
     setup(parameter_set, generate=True)
