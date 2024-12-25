189612dc458e0a508a71acc550eb164978e986c6
nanoman657 <nanoman657@gmail.com>
2024-12-25 15:10:18 -0500

    test: format muscle name

--- tests/test_OpenWormReader.py
+++ tests/test_OpenWormReader.py
@@ -28,3 +28,29 @@ class TestFormatMuscleName:
         assert (
             len(caplog.records) == log_count_expected
         ), f"The incorrect quantity of logs was recorded."
+
+    @pytest.mark.parametrize(
+        "name, name_expected, assert_msg",
+        [
+            ("ADER", "ADER", "Unknown muscle name format was incorrectly formatted"),
+            ("MVL01", "MVL01", "Known muscle name format was incorrectly formatted"),
+            (
+                "VR01",
+                "MVR01",
+                "Known muscle name format missing 'M' was incorrectly formatted",
+            ),
+            ("MDL01", "MDL01", "Muscle name with 'DL' was incorrectly formatted"),
+            ("MDR01", "MDR01", "Muscle name with 'DR' was incorrectly formatted"),
+            (
+                "MVD01",
+                "MVD01",
+                "Unknown muscle name format with 'VD' was incorrectly formatted",
+            ),
+        ],
+    )
+    def test_should_format_muscle_name(
+        self, name: str, name_expected: str, assert_msg: str, caplog
+    ) -> None:
+        caplog.set_level(logging.DEBUG)
+        name_formatted: str = format_muscle_name(name)
+        assert name_formatted == name_expected, assert_msg
